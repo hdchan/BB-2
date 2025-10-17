7a1a2fab80e1fb95463cedd64a728957c98345da
Henry Chan <henrydchan@gmail.com>
2025-10-16 21:39:38 -0400

    saving

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app, ping_interval=60, ping_timeout=120)
 
 # Dictionary to store users and their assigned rooms
-users = {}
+satellites = {}
 base_sid = []
 
 SATELLITES_ROOM_KEY = "satellites"
@@ -20,22 +20,29 @@ def index():
 @socketio.on('join')
 def handle_join():
     username = f'satellite-{request.sid}'
-    users[request.sid] = f'satellite-{request.sid}'  # Store username by session ID
+    satellites[request.sid] = f'satellite-{request.sid}'  # Store username by session ID
     join_room(SATELLITES_ROOM_KEY)
-    emit("message", f"{username} joined the chat", broadcast=True)
+    # join_room(request.sid)
+    emit("join-satellite", request.sid, room=BASE_ROOM_KEY)
+    # emit("message", f"{username} joined the chat", broadcast=True)
 
 @socketio.on('join-base')
 def handle_join():
     username = f'base-{request.sid}'
-    users[request.sid] = f'base-{request.sid}' # Store username by session ID
+    satellites[request.sid] = f'base-{request.sid}' # Store username by session ID
     base_sid.append(request.sid)
     join_room(BASE_ROOM_KEY)
     emit("message", f"{username} joined the chat", broadcast=True)
 
+@socketio.on('send-satellite')
+def handle_send_state(data):
+    # print(data)
+    emit("message", data.get('data'), broadcast=data.get('satellite'))
+
 # Handle user messages
 @socketio.on('message')
 def handle_message(data):
-    username = users.get(request.sid, "Anonymous")  # Get the user's name
+    username = satellites.get(request.sid, "Anonymous")  # Get the user's name
     if request.sid in base_sid:
         emit("message", f"{username}: {data}", room=SATELLITES_ROOM_KEY)  # Send to everyone
     else:
@@ -44,7 +51,7 @@ def handle_message(data):
 # Handle disconnects
 @socketio.on('disconnect')
 def handle_disconnect():
-    username = users.pop(request.sid, "Anonymous")
+    username = satellites.pop(request.sid, "Anonymous")
     if request.sid in base_sid:
         base_sid.remove(request.sid)
     emit("message", f"{username} left the chat", broadcast=True)
