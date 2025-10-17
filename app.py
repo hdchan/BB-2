d438af68416dcff280174a7328c75d8e1f1c6493
Henry Chan <henrydchan@gmail.com>
2025-10-16 23:57:18 -0400

    saving passing element from html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
-
+from flask_bootstrap import Bootstrap
+import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+bootstrap = Bootstrap(app)
 
 socketio = SocketIO(app, ping_interval=60, ping_timeout=120)
 
@@ -14,7 +16,14 @@ SATELLITES_ROOM_KEY = "satellites"
 BASE_ROOM_KEY = "base"
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('child.html')
+
+@app.route('/search')
+def get_external_data():
+    try:        
+        return render_template('child.html')
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
 
 # Handle new user joining
 @socketio.on('join')
@@ -40,6 +49,27 @@ def handle_send_state(data):
     emit("message", data.get('data'), broadcast=data.get('satellite'))
 
 # Handle user messages
+@socketio.on('search')
+def handle_message(data):
+    api_url = f"https://api.swu-db.com/cards/search?q=name:{data}"  # Example API
+
+    # Make a GET request to the external API
+    response = requests.get(api_url)
+
+    # Raise an exception for bad status codes (4xx or 5xx)
+    response.raise_for_status()
+
+    # Parse the JSON response
+    data = response.json()['data']
+
+    emit("search-result", data, room=request.sid)
+
+# Handle user messages
+@socketio.on('add-card')
+def handle_message(data):
+    print(data)
+
+# Handle user messages
 @socketio.on('message')
 def handle_message(data):
     username = satellites.get(request.sid, "Anonymous")  # Get the user's name
@@ -52,9 +82,9 @@ def handle_message(data):
 @socketio.on('disconnect')
 def handle_disconnect():
     username = satellites.pop(request.sid, "Anonymous")
-    if request.sid in base_sid:
-        base_sid.remove(request.sid)
-    emit("message", f"{username} left the chat", broadcast=True)
+    # if request.sid in base_sid:
+    #     base_sid.remove(request.sid)
+    # emit("message", f"{username} left the chat", broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", debug=True)
